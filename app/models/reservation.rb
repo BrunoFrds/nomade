cdad5fe843f02a6b58e91ae8c3b0ffaac721e7d7
Bruno Fernandes <brunofrdsdev@gmail.com>
2025-09-05 14:32:57 +0200

    Add reservation validates

--- app/models/reservation.rb
+++ app/models/reservation.rb
@@ -1,2 +1,5 @@
 class Reservation < ApplicationRecord
+  validates :last_name, :first_name, :email, :date, :people, presence: true
+  validates :people, numericality: { only_integer: true, greater_than: 0 }
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
 end

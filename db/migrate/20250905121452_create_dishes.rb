620dfe9592895e835f35e73b03bf49a49155fdad
Bruno Fernandes <brunofrdsdev@gmail.com>
2025-09-05 14:16:43 +0200

    Add dish model

---
+++ db/migrate/20250905121452_create_dishes.rb
@@ -0,0 +1,13 @@
+class CreateDishes < ActiveRecord::Migration[7.1]
+  def change
+    create_table :dishes do |t|
+      t.string :name
+      t.text :description
+      t.decimal :price
+      t.string :category
+      t.boolean :available
+
+      t.timestamps
+    end
+  end
+end
